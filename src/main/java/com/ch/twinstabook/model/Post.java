788f41ce7428ed6b3c37d6443321ad54051c7331
jixmxx <95912146+jixmxx@users.noreply.github.com>
2022-05-13 00:19:58 +0900

    Add files via upload
    Media,Post 모델

--- src/main/java/com/ch/twinstabook/model/Post.java
+++ src/main/java/com/ch/twinstabook/model/Post.java
@@ -2,17 +2,20 @@ package com.ch.twinstabook.model;
 
 import java.sql.Date;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 @Data
 public class Post {
-	private int postno;
-	private String origin_member_id;
-	private String member_id;
-	private String title;
-	private String content	;
-	private Date crt_date;
-	private Date mdf_date;
-	private int views;
-	private int likes;
-	private int rts;
+	private int postno;							// 게시글 번호
+	private String origin_member_id;	// 원작자
+	private String member_id;				// 작성자
+	private String content	;					// 내용
+	private Date crt_date;					// 작성일
+	private Date mdf_date;					// 수정일
+	private int views;							// 조회수
+	private int likes;								// 좋아요
+	private int rts;								// RT횟수
+	// 업로드용(input의 name과 fileName이 일치 해야함)
+	private MultipartFile file;
 }
